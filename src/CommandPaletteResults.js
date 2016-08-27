09a5c17eb9ac982d2b662cccdffefa9a86338c6b
Ivan Mattie <theguy@ivanmattie.com>
2016-08-27 11:27:34 -0600

    Added basic command results behavior, animations

--- src/CommandPaletteResults.js
+++ src/CommandPaletteResults.js
@@ -1,15 +1,27 @@
 var CommandPaletteResultsController = function(parent){
+    var self = this;
+
     this.commandPalette = parent;
     this.results = [];
 
     this.resultsContainer = document.createElement("ul");
     this.resultsContainer.className = "results";
 
+    this.resultsContainer.addEventListener("transitionend", function(e){ self.animationEnd(e); });
+
     this.commandPalette.element.appendChild(this.resultsContainer);
 };
 
-CommandPaletteResultsController.prototype.clearResults = function(){
+CommandPaletteResultsController.prototype.clearResults = function(interpolate){
+    var previousHeight = this.resultsContainer.offsetHeight;
+
     this.resultsContainer.innerHTML = "";
+
+    var targetHeight = this.resultsContainer.offsetHeight;
+
+    if(typeof interpolate === 'undefined' || interpolate){
+        this.interpolate(previousHeight, targetHeight);
+    }
 };
 
 CommandPaletteResultsController.prototype.buildResultDOM = function(command){
@@ -20,10 +32,28 @@ CommandPaletteResultsController.prototype.buildResultDOM = function(command){
     return newElement;
 };
 
+CommandPaletteResultsController.prototype.interpolate = function(start, end){
+    var self = this;
+
+    this.resultsContainer.style.height = start + "px";
+    this.resultsContainer.style.transition = "height 200ms ease-out";
+
+    requestAnimationFrame(function(){
+        self.resultsContainer.style.height = end + "px";
+    });
+};
+
+CommandPaletteResultsController.prototype.animationEnd = function(e){
+    this.resultsContainer.style.height = "";
+    this.resultsContainer.style.transition = "";
+};
+
 CommandPaletteResultsController.prototype.setResults = function(results){
+    var self = this;
+    var previousHeight = this.resultsContainer.offsetHeight;
     this.results = results;
 
-    this.clearResults();
+    this.clearResults(false);
 
     for(var r = 0; r < this.results.length; r++){
         var curr = this.results[r];
@@ -33,4 +63,8 @@ CommandPaletteResultsController.prototype.setResults = function(results){
 
         this.resultsContainer.appendChild(element);
     }
+
+    var targetHeight = this.resultsContainer.offsetHeight;
+
+    this.interpolate(previousHeight, targetHeight);
 };
