c3fd19ee1c03b09f02803c79aff56f56f85c1470
Ivan Mattie <theguy@ivanmattie.com>
2016-09-04 23:14:43 -0600

    Added basic unit test identities again

--- test/test.js
+++ test/test.js
@@ -7,4 +7,28 @@ describe("CommandPalette", function(){
     it("should have the onAll() method", function(){
         expect("function", typeof palette.onAll);
     });
+
+    it("should have the config() method", function(){
+        expect("function", typeof palette.config);
+    });
+
+    it("should have the 'properties' attribute", function(){
+        expect("object", typeof palette.properties);
+    });
+
+    describe(".properties", function(){
+
+        it("should have the 'id' attribute", function(){
+            expect(true, palette.properties && palette.properties.id);
+        });
+
+        it("should have the 'hotkey' attribute", function(){
+            expect(true, palette.properties && palette.properties.hotkey);
+        });
+
+        it("should have the 'hidden' attribute", function(){
+            expect(true, palette.properties && palette.properties.hidden);
+        });
+
+    });
 });
